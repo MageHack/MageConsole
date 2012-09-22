7cbbb4c145a2ee4f84876956f77ae9fea049aea8
Bolaji Olubajo <toluolubajo@gmail.com>
2012-09-22 14:02:07 +0100

    Added the enabled and disable flag in the admin

--- app/code/community/MageHack/MageConsole/Helper/Data.php
+++ app/code/community/MageHack/MageConsole/Helper/Data.php
@@ -1,11 +1,15 @@
 <?php
+
 /**
  * @category    MageHack
  * @package     MageHack_MageConsole
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+class MageHack_MageConsole_Helper_Data extends Mage_Core_Helper_Abstract {
 
-class MageHack_MageConsole_Helper_Data  extends Mage_Core_Helper_Abstract
-{
+    public function isEnabled() {
+        $enabled = (Mage::getStoreConfig('admin/mageconsole/enable') == 1) ? true : false;
+        return $enabled;
+    }
 
 }

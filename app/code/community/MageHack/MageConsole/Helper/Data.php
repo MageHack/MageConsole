ad8683dea7f7d062fd466e933f372320f18120f3
Narayan Varma <narayan@varma.me.uk>
2012-09-22 13:04:32 +0100

    Added Helper class'

---
+++ app/code/community/MageHack/MageConsole/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category    MageHack
+ * @package     MageHack_MageConsole
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class MageHack_MageConsole_Helper_Data  extends Mage_Core_Helper_Abstract
+{
+
+}

462de12acbb45a791b438bd18668054675fa5415
Bolaji Olubajo <toluolubajo@gmail.com>
2012-09-22 19:02:08 +0100

    Added the function to get all required attributes of the product entity

--- app/code/community/MageHack/MageConsole/Model/Request/Product.php
+++ app/code/community/MageHack/MageConsole/Model/Request/Product.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
  * @category    MageHack
  * @package     MageHack_MageConsole
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Model_Abstract implements MageHack_MageConsole_Model_Request_Interface
-{
+class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Model_Abstract implements MageHack_MageConsole_Model_Request_Interface {
 
     /**
      * Get instance of product model
      *
      * @return  Mage_Catalog_Model_Produt
      */
-    protected function _getModel()
-    {
+    protected function _getModel() {
         return Mage::getModel('catalog/product');
     }
 
@@ -25,8 +24,8 @@ class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Mo
     public function add() {
         var_dump('called');
         $this->setType(self::RESPONSE_TYPE_PROMPT);
-        $this->setMessage(array('Hello', 'World'));
-        
+        $this->setMessage($this->getReqAttr());
+
         return $this;
     }
 
@@ -47,7 +46,7 @@ class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Mo
     public function remove() {
         
     }
-    
+
     /**
      * Show command
      *
@@ -55,25 +54,25 @@ class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Mo
      */
     public function show() {
         $collection = $this->_getModel()
-            ->getCollection()
-            ->addAttributeToSelect('*');
-                    
+                ->getCollection()
+                ->addAttributeToSelect('*');
+
         foreach ($this->getConditions() as $condition) {
             $collection->addFieldToFilter($condition['attribute'], array($condition['operator'] => $condition['value']));
         }
-        
+
         if (!$collection->count()) {
-            $message    = 'No match found';
+            $message = 'No match found';
         } else if ($collection->count() > 1) {
-            $message    = 'Multiple matches found, use the list command';
+            $message = 'Multiple matches found, use the list command';
         } else {
-            $product    = $collection->getFirstItem();
-            $message    = sprintf('Name: %s', $product->getName());
+            $product = $collection->getFirstItem();
+            $message = sprintf('Name: %s', $product->getName());
         }
-        
+
         $this->setType(self::RESPONSE_TYPE_MESSAGE);
         $this->setMessage($message);
-        
+
         return $this;
     }
 
@@ -93,8 +92,22 @@ class MageHack_MageConsole_Model_Request_Product extends MageHack_MageConsole_Mo
      *
      */
     public function help() {
-	$this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
         $this->setMessage('help was requested for a product - this is the help message');
-	return $this;
+        return $this;
     }
+
+    /**
+     * Get all required attributes of the product entity
+     * @return array
+     */
+    public function getReqAttr() {
+        $sql = "SELECT e.attribute_code, e.frontend_label FROM `eav_attribute` as e WHERE entity_type_id IN(select eat.entity_type_id FROM eav_entity_type as eat where eat.entity_type_code = 'catalog_product') and e.is_required =1 and frontend_label is not null";
+
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_read');
+
+        $attributes = $connection->fetchAll($sql);
+        return $attributes;
+    }
+
 }

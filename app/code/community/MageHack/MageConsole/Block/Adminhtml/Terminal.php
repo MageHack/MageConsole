037ed3c98ee1b7b665dd669606eb0286811b0025
Narayan Varma <narayan@varma.me.uk>
2012-09-22 15:26:15 +0100

    MAG-8 Hook up the autocomplete with controller

--- app/code/community/MageHack/MageConsole/Block/Adminhtml/Terminal.php
+++ app/code/community/MageHack/MageConsole/Block/Adminhtml/Terminal.php
@@ -7,5 +7,36 @@
 
 class MageHack_MageConsole_Block_Adminhtml_Terminal extends Mage_Adminhtml_Block_Abstract
 {
+    /**
+     * get Command submission URL
+     *
+     * @return string
+     */
+
+    public function getSubmitUrl()
+    {
+        return Mage::helper('adminhtml')->getUrl('adminhtml/mageconsole/submit');
+    }
+
+
+    /**
+     * get Autocomplete URL
+     *
+     * @return string
+     */
+    public function getAutocompleteUrl()
+    {
+        return Mage::helper('adminhtml')->getUrl('adminhtml/mageconsole/autocomplete');
+    }
+
+    /**
+     * Retrieve Session Form Key
+     *
+     * @return string
+     */
+    public function getFormKey()
+    {
+        return Mage::getSingleton('core/session')->getFormKey();
+    }
 
 }

001bdd9d423774eb70f2959df6a536beb57e9771
Narayan Varma <narayan@varma.me.uk>
2012-09-23 08:46:48 +0100

    MAG-10 Entity: category / MAG-32 Method: show

---
+++ app/code/community/MageHack/MageConsole/Model/Request/Catalog/Category.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @category    MageHack
+ * @package     MageHack_MageConsole
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MageHack_MageConsole_Model_Request_Catalog_Category
+    extends MageHack_MageConsole_Model_Abstract
+    implements MageHack_MageConsole_Model_Request_Interface
+{
+
+    /**
+     * Get instance of Customer model
+     *
+     * @return  Mage_Customer_Model_Customer
+     */
+    protected function _getModel()
+    {
+        return Mage::getModel('catalog/category');
+    }
+
+    /**
+     * Add command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function add()
+    {
+
+    }
+
+    /**
+     * Update command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     * Remove command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function remove()
+    {
+
+    }
+
+    /**
+     * Show command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function show()
+    {
+        $collection = $this->_getMatchedResults();
+
+        if (!$collection->count()) {
+            $message    = 'No match found';
+        } else if ($collection->count() > 1) {
+            $message    = 'Multiple matches found, please use the list command';
+        } else {
+            $category    = $collection->getFirstItem();
+            $details = $category->getData();
+            $message = array();
+            foreach ($details as $key => $info) {
+                $message[] = sprintf('%s: %s', $key, $info);
+            }
+        }
+
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage($message);
+
+        return $this;
+    }
+
+    /**
+     * List command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function listing()
+    {
+
+    }
+
+    /**
+     * Help command
+     *
+     * @return MageHack_MageConsole_Model_Abstract
+     *
+     */
+    public function help()
+    {
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage('help was requested for a product - this is the help message');
+        return $this;
+    }
+}

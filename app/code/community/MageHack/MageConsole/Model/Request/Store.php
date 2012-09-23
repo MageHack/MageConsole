3d94ce075933ac96c27e21d578cf5fb9cafce54b
Szabolcs Ban <sab@storesco.com>
2012-09-23 15:12:00 +0100

    MAG-70 store handling

--- app/code/community/MageHack/MageConsole/Model/Request/Store.php
+++ app/code/community/MageHack/MageConsole/Model/Request/Store.php
@@ -4,19 +4,30 @@
  * @package     MageHack_MageConsole
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class MageHack_MageConsole_Model_Request_Store
-    extends MageHack_MageConsole_Model_Abstract
-    implements MageHack_MageConsole_Model_Request_Interface
+class MageHack_MageConsole_Model_Request_Store extends MageHack_MageConsole_Model_Abstract
 {
 
-//    protected $_attrToShow = array(
-//        'store' => 'store',
-//        'config' => 'config',
-//    );
-//
-//    protected $_columnWidths = array(
-//        'columnWidths' => array(12, 40)
-//    );
+    /**
+     * Columns
+     *
+     * @var     array
+     */
+    protected $_columns = array(
+        'store_id'		=> 12,
+        'website_id'  	=> 12,
+        'name'        	=> 30,
+        'is_active'		=> 12,
+    );
+
+    /**
+     * Get instance of Config model
+     *
+     * @return  Mage_Core_Model_Config
+     */
+    protected function _getModel()
+    {
+        return Mage::getModel('core/store');
+    }
 
     /**
      * Set store command
@@ -25,25 +36,56 @@ class MageHack_MageConsole_Model_Request_Store
      */
     public function set()
     {
-        $this->setType(self::RESPONSE_TYPE_PROMPT);
-        $values = array();
-        foreach (Mage::app()->getStores() as $store) {
-            $values[] = array('value' => $store->getId(), 'label'=>$store->getName());
+        $param = $this->getRequest(2);
+        if (strtolower($param) == 'default') {
+            $defaultStoreId = Mage::app()
+                ->getWebsite()
+                ->getDefaultGroup()
+                ->getDefaultStoreId();
+            $store = Mage::app()->getStore($defaultStoreId);
         }
-        $this->setMessage(array('store'=>array('label' => 'Set store to:', 'values' => $values)));
+        elseif (is_numeric($param)) {
+            $store = Mage::app()->getStore($param);
+        }
+        else {
+            foreach (Mage::app()->getStores() as $object) {
+                if (strtolower($object->getName()) == strtolower($param)) {
+                    $store = $object;
+                }
+            }
+        }
+
+        if (!isset($store)) {
+            $this->setType(self::RESPONSE_TYPE_MESSAGE);
+            $this->setMessage("No store found!");
+        } else {
+            Mage::getSingleton('adminhtml/session')->setMageConsoleStore($store);
+        }
         return $this;
     }
 
     /**
-     * Save command
+     * Get store command
      *
-     * @return  MageHack_MageConsole_Model_Abstract
+     * @return  Mage_Core_Model_Store
      */
-    public function prompt()
+    public function get()
     {
+        if (!$store = Mage::getSingleton('adminhtml/session')->getMageConsoleStore()) {
+            $store = Mage::app()->getStore();
+        }
+        $data = $store->getData();
+        $value = array();
+        foreach ($this->_columns as $attr => $width) {
+            $value[$attr] = $data[$attr];
+        }
+        $_values = array($value);
 
+        $message = Mage::helper('mageconsole')->createTable($_values, true, array('columnWidths' => array_values($this->_columns)));
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage($message);
+        return $this;
     }
-
 
     /**
      * Help command

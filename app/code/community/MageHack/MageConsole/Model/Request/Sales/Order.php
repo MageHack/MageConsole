5ca8222de1989acfadb66f54b0762560e7be253d
Marcell Kiss-Toth <marcellk@marcellk.com>
2012-09-23 11:28:00 +0100

    MAG-15 Entity: order / MAG-40 Method: listing

--- app/code/community/MageHack/MageConsole/Model/Request/Sales/Order.php
+++ app/code/community/MageHack/MageConsole/Model/Request/Sales/Order.php
@@ -9,15 +9,20 @@ class MageHack_MageConsole_Model_Request_Sales_Order
     implements MageHack_MageConsole_Model_Request_Interface
 {
 
-    protected $_attrToShow = array(
-        'increment_id' => 'increment_id',
-        'customer_email' => 'customer_email',
-        'status' => 'status',
-        'created_at' => 'created_at'
-    );
-
-    protected $_columnWidths = array(
-        'columnWidths' => array(12, 40, 10, 20)
+    /**
+     * Columns
+     *
+     * @var     array
+     */
+    protected $_columns = array(
+        'increment_id'          => 15,
+        'customer_firstname'    => 20,
+        'customer_lastname'     => 20,
+        'customer_email'        => 30,
+        'grand_total'           => 15,
+        'status'                => 10,
+        'order_currency_code'   => 22,
+        'created_at'            => 22,
     );
 
     /**
@@ -37,8 +42,8 @@ class MageHack_MageConsole_Model_Request_Sales_Order
      */
     public function add()
     {
-        $this->setType(self::RESPONSE_TYPE_PROMPT);
-        $this->setMessage(array('Hello', 'World'));
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage('This action is not available');
         return $this;
     }
 
@@ -49,7 +54,9 @@ class MageHack_MageConsole_Model_Request_Sales_Order
      */
     public function update()
     {
-
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage('This action is not available');
+        return $this;
     }
 
     /**
@@ -118,7 +125,7 @@ class MageHack_MageConsole_Model_Request_Sales_Order
     {
         $collection = $this->_getMatchedResults();
 
-        foreach ($this->_attrToShow as $attr) {
+        foreach ($this->_columns as $attr => $width) {
             $collection->addAttributeToSelect($attr);
         }
 
@@ -129,10 +136,16 @@ class MageHack_MageConsole_Model_Request_Sales_Order
             if (is_array($values) && $values['totalRecords'] > 0) {
                 foreach ($values['items'] as $row) {
                     if (is_array($row)) {
-                        $_values[] = (array_intersect_key($row, $this->_attrToShow));
+                        $value = array();
+
+                        foreach ($this->_columns as $attr => $width) {
+                            $value[$attr] = $row[$attr];
+                        }
+
+                        $_values[] = $value;                        
                     }
                 }
-                $message = Mage::helper('mageconsole')->createTable($_values, true, $this->_columnWidths);
+                $message = Mage::helper('mageconsole')->createTable($_values, true, array('columnWidths' => array_values($this->_columns)));
             }
         }
 

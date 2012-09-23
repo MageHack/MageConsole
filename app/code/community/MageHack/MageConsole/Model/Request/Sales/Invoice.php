3c2f67fba3db0861ff8e7fcea76b7bca6b266637
Narayan Varma <narayan@varma.me.uk>
2012-09-23 10:10:33 +0100

    MAG-14 Entity: invoice / MAG-52 Method: remove MAG-14 Entity: invoice / MAG-53 Method: show MAG-14 Entity: invoice / MAG-54 Method: listing

---
+++ app/code/community/MageHack/MageConsole/Model/Request/Sales/Invoice.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @category    MageHack
+ * @package     MageHack_MageConsole
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MageHack_MageConsole_Model_Request_Sales_Invoice
+    extends MageHack_MageConsole_Model_Abstract
+    implements MageHack_MageConsole_Model_Request_Interface
+{
+
+    protected $_attrToShow = array(
+        'increment_id' => 'increment_id',
+        'order_id' => 'order_id',
+        'grand_total' => 'grand_total',
+        'created_at' => 'created_at'
+    );
+
+    protected $_columnWidths = array(
+        'columnWidths' => array(12, 40, 10, 20)
+    );
+
+    /**
+     * Get instance of Customer model
+     *
+     * @return  Mage_Customer_Model_Customer
+     */
+    protected function _getModel()
+    {
+        return Mage::getModel('sales/order_invoice');
+    }
+
+    /**
+     * Add command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function add()
+    {
+        $this->setType(self::RESPONSE_TYPE_PROMPT);
+        $this->setMessage(array('Hello', 'World'));
+        return $this;
+    }
+
+    /**
+     * Update command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     * Remove command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function remove()
+    {
+        $message = 'Sorry, delete operation not supported on Invoice. Please try deleting the order itself.';
+        $this->setType(self::RESPONSE_TYPE_ERROR);
+        $this->setMessage($message);
+        return $this;
+    }
+
+    /**
+     * Show command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function show()
+    {
+        $collection = $this->_getMatchedResults();
+
+        if (!$collection->count()) {
+            $message    = 'No match found';
+        } else if ($collection->count() > 1) {
+            $message    = 'Multiple matches found, please use the list command';
+        } else {
+            $invoice    = $collection->getFirstItem();
+            $details = $invoice->getData();
+            $message = array();
+            foreach ($details as $key => $info) {
+                $message[] = sprintf('%s: %s', $key, $info);
+            }
+        }
+
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage($message);
+
+        return $this;
+    }
+
+    /**
+     * List command
+     *
+     * @return  MageHack_MageConsole_Model_Abstract
+     */
+    public function listing()
+    {
+        $collection = $this->_getMatchedResults();
+
+        foreach ($this->_attrToShow as $attr) {
+            $collection->addAttributeToSelect($attr);
+        }
+
+        if (!$collection->count()) {
+            $message = 'No match found';
+        } else if ($collection->count() > 0) {
+            $values = $collection->toArray();
+            if (is_array($values) && $values['totalRecords'] > 0) {
+                foreach ($values['items'] as $row) {
+                    if (is_array($row)) {
+                        $_values[] = (array_intersect_key($row, $this->_attrToShow));
+                    }
+                }
+                $message = Mage::helper('mageconsole')->createTable($_values, true, $this->_columnWidths);
+            }
+        }
+
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage($message);
+        return $this;
+    }
+
+    /**
+     * Help command
+     *
+     * @return MageHack_MageConsole_Model_Abstract
+     *
+     */
+    public function help()
+    {
+        $this->setType(self::RESPONSE_TYPE_MESSAGE);
+        $this->setMessage('help was requested for a product - this is the help message');
+        return $this;
+    }
+}
